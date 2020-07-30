b27fd17a16e1c200ad0beb32837e740ea654f511
Shawn Davis <shawn@ptltd.co>
2020-07-30 08:41:10 -0400

    Added tests.

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+from datetime_machine.utils import *
+
+
+def test_get_days_in_month():
+    pass
+
+
+def test_increment():
+    pass
+
+
+def test_is_business_day():
+    pass
+
+
+def test_is_holiday():
+    pass
+
+
+def test_is_leap_year():
+    assert is_leap_year(2019) is False
+    assert is_leap_year(2020) is True
